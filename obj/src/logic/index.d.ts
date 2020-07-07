c562fbd825d2102abe3b7834b6d9155bef93c353
Denis Kuznetsov <deniskuzn@gmail.com>
2020-07-07 18:12:18 +0300

    added payment platform classes export

--- obj/src/logic/index.d.ts
+++ obj/src/logic/index.d.ts
@@ -1,2 +1,5 @@
 export { IPaymentsController } from './IPaymentsController';
 export { PaymentsController } from './PaymentsController';
+export { IPaymentPlatform } from './platforms/IPaymentPlatform';
+export { PayPalPlatform } from './platforms/PayPalPlatform';
+export { StripePlatform } from './platforms/StripePlatform';
--- src/logic/index.ts
+++ src/logic/index.ts
@@ -1,2 +1,6 @@
 export { IPaymentsController } from './IPaymentsController';
 export { PaymentsController } from './PaymentsController';
+
+export { IPaymentPlatform } from './platforms/IPaymentPlatform';
+export { PayPalPlatform } from './platforms/PayPalPlatform';
+export { StripePlatform } from './platforms/StripePlatform';
